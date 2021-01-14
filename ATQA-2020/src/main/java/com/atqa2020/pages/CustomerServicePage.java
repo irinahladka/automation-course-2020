c5e8947eb79ba0925ba98fce3e8e306217049c9e
Oleksandr Perepelytsya <o.perepelytsya@gmail.com>
2021-01-14 13:35:54 +0000

    Update CustomerServicePage.java

--- ATQA-2020/src/main/java/com/atqa2020/pages/CustomerServicePage.java
+++ ATQA-2020/src/main/java/com/atqa2020/pages/CustomerServicePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class CustomerServicePage extends Page{
-
+        //TODO please remove redundant empty lines
 
         private static String CONTACT_PAGE_TITLE = "Top Customer Questions";
         private String contactPageTitleLocator = "//div[@class=\"contentHeadTop\"]";
