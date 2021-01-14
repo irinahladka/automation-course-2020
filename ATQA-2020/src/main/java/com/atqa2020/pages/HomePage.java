61050c64f5542eff99d1bc61e764b4bbd8539ae0
Oleksandr Perepelytsya <o.perepelytsya@gmail.com>
2021-01-14 13:38:18 +0000

    Update HomePage.java

--- ATQA-2020/src/main/java/com/atqa2020/pages/HomePage.java
+++ ATQA-2020/src/main/java/com/atqa2020/pages/HomePage.java
@@ -8,7 +8,7 @@ public class HomePage extends Page {
 
     private Header header;
     private Footer footer;
-
+    //TODO empty lines
 
     public HomePage(RemoteWebDriver driver) {
         super(driver);
@@ -23,7 +23,7 @@ public class HomePage extends Page {
     public <T extends Page> T searchForItem(Class<T> clazz, String searchTerm) throws Exception {
         return header.setSearchTerm(searchTerm).clickOnGirlsButton(clazz);
     }
-
+    //TODO it feels like this method should be navigateToGirlsPage()
     public GirlsPage navigateToGirlsButton() throws Exception {
         return header.navigateToGirlsButton();
 
